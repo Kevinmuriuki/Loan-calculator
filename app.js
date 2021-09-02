e8a2d8fc99861b1fecb255eaada0b7d272541d84
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-09-02 16:01:03 +0300

    added some modifications

---
+++ app.js
@@ -0,0 +1,30 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log('calculating...');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('monthly-payment');
+  const totalInterst = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat('years.value') * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly = calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+  }
+
+  e.preventDefault;
+}
