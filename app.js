7210568735bcd596826b3d16d82bb51318b5db98
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-09-08 12:47:48 +0300

    added the loader and some other modificatrions

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log('calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
-  const interest = document.getElementById('years');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
   const monthlyPayment = document.getElementById('monthly-payment');
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
@@ -23,15 +34,23 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    //Show result
+    document.getElementById('results').style.display = 'block';
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  //Hide result
+  document.getElementById('results').style.display = 'none';
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
   
