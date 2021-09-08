dfc0087a227390650aac5682a39c8eec10df69d2
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-09-08 12:10:12 +0300

    added all the mathematical functionalities for calculations

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ function calculateResults(e) {
   console.log('calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
-  const interest = document.getElementById('interest');
-  const years = document.getElementById('monthly-payment');
-  const totalInterst = document.getElementById('total-interest');
+  const interest = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
 
   const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat('years.value') * 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 
   // Compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
@@ -20,11 +21,14 @@ function calculateResults(e) {
 
   if(isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly = calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
   } else {
     console.log('Please check your numbers');
   }
 
-  e.preventDefault;
+
+
+  
+  e.preventDefault();
 }
