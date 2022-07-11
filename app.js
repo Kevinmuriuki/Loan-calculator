a9918f97472bdef5443786a99b24c8669d46b141
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-07-11 14:28:50 +0300

    Add changes

--- app.js
+++ app.js
@@ -6,34 +6,43 @@ document.getElementById('loan-form').addEventListener('submit', function(e) {
   // Show loader
   document.getElementById('loading').style.display = 'block';
 
-  setTimeout(calculateResults, 2000);
+  setTimeout(calculateResults, 500);
 
   e.preventDefault();
 });
 
 // Calculate Results
 function calculateResults() {
-  console.log('calculating...');
   // UI Vars
-  const amount = document.getElementById('amount');
+  const initialValue = document.getElementById('initial-value');
+  const topUp = document.getElementById('top-up');
+  const interestRate = document.getElementById('interest-rate');
+  const months = document.getElementById('months');
+  
+  const investedAmount = document.getElementById('invested-amount');
   const interest = document.getElementById('interest');
-  const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
+  const tax = document.getElementById('tax');
+  const totalInvestment = document.getElementById('total-investment');
 
-  const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
+  // Convertion
+  const period = parseFloat(months.value);
+  const principal = (parseFloat(initialValue.value) + parseFloat(topUp.value)).toFixed(2);
+  const totalPrincipal = ((parseFloat(initialValue.value) + parseFloat(topUp.value)) * period).toFixed(2)
+  const rate = (parseFloat(interestRate.value) / 1200);
+  
 
   // Compute monthly payment
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal*x*calculatedInterest)/(x-1);
+  const x = Math.pow((1 + rate / period), 1);
+  const monthly = ((principal * x)).toFixed(2);
+  console.log(monthly);
+  const interestValue = (parseFloat(totalPrincipal) + parseFloat((monthly - principal))).toFixed(2);
+  // console.log(interestValue);
 
   if(isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    investedAmount.value = principal;
+    interest.value = (monthly - principal).toFixed(2);
+    tax.value = ((interest.value * 15) / 100).toFixed(2);
+    totalInvestment.value = (interestValue - parseFloat(tax.value)).toFixed(2);
 
     //Show result
     document.getElementById('results').style.display = 'block';
